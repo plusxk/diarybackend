52aba0d737efb97d7de5b47ebda9dca20252c409
genewang0929 <genewang7@gmail.com>
2021-12-07 20:47:56 +0800

    no message

--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -9,9 +9,6 @@ router.get('/folder', folderController.getAllFolder);
 //GET specific folder by folderID
 router.get('/folder/:folderID', folderController.getFolderByID);
 
-//TODO: GET diary by in specific folder by folderID
-// router.get('/folder', diaryController.postDiary);
-
 //POST folder
 router.post('/folder', folderController.postFolder);
 
