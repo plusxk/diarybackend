3cf2c1290050a4307d39caa23930360617643b18
genewang0929 <genewang7@gmail.com>
2021-12-07 11:26:29 +0800

    feat- get folder by id

--- controller/folder.js
+++ controller/folder.js
@@ -1,12 +1,27 @@
 const User = require('../model/userDBSchema');
 
 //取得所有資料夾
-exports.getFolder = (req, res) => {
+exports.getAllFolder = (req, res) => {
     User.find({userID: '1'}, (err, docs) => {
         if (err)
             console.log(err);
         res.status(500).json(docs[0].toObject().folder);
     });
+};
+
+//取得資料夾(依據folderID)
+exports.getFolderByID = (req, res) => {
+    User.find({userID: '1'}, (err, docs) => {
+        if (err)
+            console.log(err);
+
+        const folders = docs[0].toObject().folder;
+        const folder = folders.find((item, index, array) => {
+            return item.folderID === req.params.folderID;
+        });
+        res.status(500).json({ folder });
+    });
+
 };
 
 //新增資料夾
--- routes/folderRoutes.js
+++ routes/folderRoutes.js
@@ -4,9 +4,12 @@ const folderController = require('../controller/folder');
 const router = express.Router();
 
 //GET all folders
-router.get('/folder', folderController.getFolder);
+router.get('/folder', folderController.getAllFolder);
 
-//POST folder
+//GET specific folder by folderID
+router.get('/folder/:folderID', folderController.getFolderByID)
+
+//POST folderr
 router.post('/folder', folderController.postFolder);
 
 module.exports = router;
