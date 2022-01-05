60457cacc37e28437c6a2baa370669d7bc42f365
Yao <elites96105@gmail.com>
2022-01-05 23:38:28 +0800

    feat: modify fileuploadRoutes

--- routes/fileuploadRoutes.js
+++ routes/fileuploadRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const fileUploadController = require('../controller/fileUpload');
+const fileUploadController = require('../controller/fileupload');
 const authController = require('../controller/auth');
 const router = express.Router();
 const upload=multer();
